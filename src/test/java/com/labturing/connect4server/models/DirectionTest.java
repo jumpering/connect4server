3c4923360297fe5a3c6f791d4d40d10b89880562
Xavi Gómez Canals <jumpering@gmail.com>
2023-07-10 17:59:17 +0200

    test modify from github webapp

---
+++ src/test/java/com/labturing/connect4server/models/DirectionTest.java
@@ -0,0 +1,13 @@
+package com.labturing.connect4server.models;
+
+import org.junit.jupiter.api.Test;
+
+public class DirectionTest {
+
+    private Direction direction;
+
+    @Test
+    public void givenDirectionWhenGetOppositeThenReturnOpposite(){
+        
+    }
+}
